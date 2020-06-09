0f310c614c655a21bf19da0c84c380435d988c79
jattoabdul <jattoade@gmail.com>
2020-06-09 06:53:22 +0100

    fix: revert query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@
 
 class HomeController < AuthenticatedController
   def index
-    @products = ShopifyAPI::Product.find(:all, params: { limit: 10, title: 'narwhal' })
+    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
