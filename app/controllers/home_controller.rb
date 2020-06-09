d0d08e9b9c007571529adc2e8fc646894c6d43af
jattoabdul <jattoade@gmail.com>
2020-06-09 06:49:41 +0100

    add: modify the query params for products

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@
 
 class HomeController < AuthenticatedController
   def index
-    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+    @products = ShopifyAPI::Product.find(:all, params: { limit: 10, title: 'narwhal' })
+    @orders = ShopifyAPI::Order.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
 end
