3be1e7e9f4d96648f9be3c2ebf8117ba9d9e3e5d
jattoabdul <jattoade@gmail.com>
2020-06-10 01:35:17 +0100

    hotfix: get shop domain using the shopify admin api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@
 
 class HomeController < AuthenticatedController
   def index
-    shop = Shop.find_by(shopify_domain: params[:shop])
+    shop_domain = ShopifyAPI::Shop.current.domain
+    shop = Shop.find_by(shopify_domain: shop_domain)
     @orders = NarwhalOrder.where(shop: shop.id)
   end
 end
